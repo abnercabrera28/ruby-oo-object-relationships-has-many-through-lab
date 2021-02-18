6d8218f164ec7911af2abac5ecea32b7b68dc0b5
Abner <abnercabrera28@gmail.com>
2021-02-18 14:59:46 -0500

    commit tree 180163f76fa202116e19c3c3d0acfb3f34a68dbb

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
+class Doctor
 
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+       @@all 
+    end
+
+    def appointments
+        Appointment.all.select do |app|
+            app.doctor == self
+        end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        appointments.map do |app|
+            app.patient
+        end
+    end
+
+end
